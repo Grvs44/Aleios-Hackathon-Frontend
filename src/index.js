91d18c849962edec20da4f7426651a6593193f25
Joe Greaves <74414406+Grvs44@users.noreply.github.com>
2023-02-12 16:57:31 +0000

    Added service worker so works offline (production build only) Will become an installable PWA with different icon/image sizes added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
+import { registerServiceWorker } from './serviceWorkerRegistration';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
@@ -12,3 +13,5 @@ root.render(
     <App />
   </StrictMode>
 );
+
+registerServiceWorker();
